6e3a6a1fd5c3e7be98b81a5636f726709dd84080
arammkrtchyan <aram.mkrtchyan.87@gmail.com>
2015-05-15 03:04:40 +0400

    na posledok

--- server/api/event/EventHandler.js
+++ server/api/event/EventHandler.js
@@ -1,1 +1,10 @@
+var redis = require("redis"),
+redisClient = redis.createClient(6379, '75.126.39.90');
 
+redisClient.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+redisClient.on('pmessage', function (pattern, channel, message) {
+
+});
