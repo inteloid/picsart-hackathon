aaa7552b5cbd3337f90188f2f009fa3e4547a934
harut <harut.martirosyan@gmail.com>
2015-05-14 22:58:38 +0400

    added pin handlers

--- raspberry-pi.py
+++ raspberry-pi.py
@@ -12,7 +12,15 @@ def on_gauge(*args):
 	duty = angle / 10.0 + 2.5
 	pwm.ChangeDutyCycle(duty)
 
+def on_pin1(*args):
+	print(args)
+
+def on_pin2(*args):
+	print(args)
+
 socketIO = SocketIO('fra-cluster2', 3300, LoggingNamespace)
 socketIO.on('gauge', on_gauge)
+socketIO.on('pin1', on_pin1)
+socketIO.on('pin2', on_pin2)
 socketIO.wait(seconds=1000)
 GPIO.cleanup()
